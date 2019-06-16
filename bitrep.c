e7576382c5c6effe95bcfea265c20f90b5606db7
Dragos <dragos.rugescu@yahoo.com>
2019-06-16 16:45:15 +0300

    Memory representation of things

---
+++ bitrep.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+typedef unsigned char* byte_pointer; 
+  
+/*show bytes takes byte pointer as an argument 
+  and prints memory contents from byte_pointer 
+  to byte_pointer + len */
+void show_bytes(byte_pointer start, int len)  
+{ 
+     int i; 
+     for (i = 0; i < len; i++) 
+           printf(" %.2X", start[i]); 
+
+     printf("\n"); 
+} 
+  
+void show_int(int x) 
+{ 
+     show_bytes((byte_pointer) &x, sizeof(int)); 
+} 
+  
+void show_float(float x)  
+{ 
+     show_bytes((byte_pointer) &x, sizeof(float)); 
+} 
+  
+void show_pointer(void *x)  
+{ 
+     show_bytes((byte_pointer) &x, sizeof(void *)); 
+} 
+  
+/* Driver program to test above functions */
+int main() 
+{ 
+    int i = 256; 
+    float f = 1.0; 
+    int *p = &i; 
+
+    printf("show_float(%f) = ", f);
+    show_float(f); 
+
+    printf("show_int(%d) = ", i);
+    show_int(i); 
+
+    printf("show_pointer(%p) = ", p);
+    show_pointer(p); 
+
+    return 0; 
+} 
--- structs.c
+++ structs.c
@@ -20,11 +20,20 @@ union b {
 	/* padded to 8 with additional 3 */
 };
 
+struct c {
+	int a : 16; /* 16 bits - 2 bytes */
+	int d : 17; /* 17 bits - 2 bytes + 1 bit total */
+	/* padding to multiple of 4 bytes */
+	/* if d is 16 bits, sizeof(c) is 4, if d is 17, sizeof(c) is 8*/
+};
+
 void main()
 {
 	struct a alpha;
 	union b beta;	
+	struct c charlie;
 
 	printf("sizeof(alpha) = %lu\n", sizeof(alpha));
 	printf("sizeof(beta) = %lu\n", sizeof(beta));
+	printf("sizeof(charlie) = %lu\n", sizeof(charlie));
 }

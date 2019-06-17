4b69fba10916776349e14325e39fd62f52da8afa
Dragos <dragos.rugescu@yahoo.com>
2019-06-17 23:46:56 +0300

    DFS/BFS & linked list

---
+++ dfsbfs.c
@@ -0,0 +1,224 @@
+/* DFS, BFS */
+#include <stdio.h>
+
+/* bfs for shortest path between two points */
+/* also dijkstra and a light astar */
+
+#define		MAX						6
+#define 	ERROR					0
+#define		OK						1
+#define 	CARDINAL_DIRECTIONS		4
+#define		VISITED 				1
+#define		NOT_VISITED				0
+#define 	ADJACENT 				1
+#define		NOT_ADJACENT 			0
+
+int visited[MAX][MAX] = { NOT_VISITED };
+int stepmat[MAX][MAX] = { 0 };
+
+//queue variables
+int queuex[MAX * MAX + 1];
+int queuey[MAX * MAX + 1];
+int rear = -1;
+int front = 0;
+int queueItemCount = 0;
+
+void insert(int datax, int datay) {
+   rear++;
+   queuex[rear] = datax;
+   queuey[rear] = datay;
+   queueItemCount++;
+}
+
+int peekData(int* tempx, int* tempy) {
+	*tempx = queuex[front];
+	*tempy = queuey[front];
+}
+
+void removeData() {
+   queueItemCount--;
+   front++;
+}
+
+int isQueueEmpty() {
+   return queueItemCount == 0;
+}
+
+void print_mat(int m[][MAX]) {
+	printf("Matrix:\n");
+
+	for(int i = 0; i < MAX; i++) {
+		printf("{ ");
+		for (int j = 0; j < MAX; j++) {
+			printf("%d  ", m[i][j]);
+		}
+		printf("}\n");
+	}
+}
+
+void print_helper(int m[][MAX], int initx, int inity, int targetx, int targety) {
+	printf("Matrix:\n");
+
+	for(int i = 0; i < MAX; i++) {
+		printf("{ ");
+		for (int j = 0; j < MAX; j++) {
+			if ((initx == i) && (inity == j))
+				printf("S  ");
+			else if ((targetx == i) && (targety == j))
+				printf("X  ");
+			else
+				printf("%d  ", m[i][j]);
+		}
+		printf("}\n");
+	}
+}
+
+int check(int x, int y) {
+	if (x < 0)
+		return ERROR;
+
+	if (x > 5)
+		return ERROR;
+
+	if (y < 0)
+		return ERROR;
+
+	if (y > 5)
+		return ERROR;
+
+	if (visited[x][y] == VISITED)
+		return ERROR;
+
+	return OK;
+}
+
+int adjacent(int m[][MAX], int i, int j, int tempx, int tempy) {
+	int c = (OK == check(tempx + i, tempy + j));
+
+	if (m[tempx + i][tempy + j] == 0)
+		return NOT_ADJACENT;
+
+	if (c) {
+		//printf("%d,%d adjacent to %d, %d\n", tempx + i, tempy + j, tempx, tempy);
+		return ADJACENT;
+	}
+
+	return NOT_ADJACENT;
+}
+
+int getAdjUnvisitedVertex(int m[][MAX], int tempx, int tempy, int* newx, int* newy) {
+   int i,j;
+
+   for (i = -1; i <= 1; i++) {
+      for (j = -1; j <= 1; j++) {
+         if(adjacent(m, i, j, tempx, tempy) == ADJACENT && (visited[tempx + i][tempy + j] == NOT_VISITED)) {
+         	*newx = tempx + i;
+         	*newy = tempy + j;
+            return OK;
+         }
+      }
+   }
+
+   return ERROR;
+}
+
+void dfs(int m[][MAX], int initx, int inity, int targetx, int targety, int steps) {
+	int Ex = initx;
+	int Ey = inity + 1; 
+
+	int Wx = initx;
+	int Wy = inity - 1; 
+
+	int Sx = initx + 1;
+	int Sy = inity; 
+
+	int Nx = initx - 1;
+	int Ny = inity; 
+
+	int nextX[] = {Ex, Wx, Sx, Nx};
+	int nextY[] = {Ey, Wy, Sy, Ny};
+
+#ifdef DEBUG
+	printf("dFS %d,%d\n", initx, inity);
+#endif
+
+	if ((initx == targetx) && (inity == targety))
+	{
+		printf("Target found in %d steps.\n", steps);
+	}
+
+	for (int i = 0; i < CARDINAL_DIRECTIONS; i++) {
+		if (OK == check(nextX[i], nextY[i])) {
+			visited[nextX[i]][nextY[i]] = VISITED;
+			dfs(m, nextX[i], nextY[i], targetx, targety, steps + 1);
+		}
+	}
+}
+
+void bfs(int m[][MAX], int initx, int inity, int targetx, int targety, int steps) {
+
+	int tempx, tempy;
+	int newx, newy;
+	int i, j;
+
+	if ((initx == targetx) && (inity == targety))
+	{
+		printf("Target found in %d steps.\n", steps);
+	}
+
+	/* mark first node as visited */
+	visited[initx][inity] = VISITED;
+
+	insert(initx, inity);
+	//printf("Pushed (%d,%d).\n\n", initx, inity);
+	stepmat[initx][inity] = 1;
+
+	while(!isQueueEmpty()) {
+		//get the unvisited vertex of vertex which is at front of the queue
+		peekData(&tempx, &tempy);
+		removeData();
+		//printf("Popped (%d,%d), steps = %d.\n", tempx, tempy, stepmat[tempx][tempy]);
+
+		//no adjacent vertex found
+		while(ERROR != getAdjUnvisitedVertex(m, tempx, tempy, &newx, &newy)) {    
+			//printf("Pushed (%d,%d) with steps = %d.\n", newx, newy, stepmat[tempx][tempy] + 1);
+
+        	visited[newx][newy] = VISITED;
+        	insert(newx, newy);
+        	stepmat[newx][newy] = stepmat[newx][newy] + stepmat[tempx][tempy] + 1;
+
+			if ((newx == targetx) && (newy == targety))
+			{
+				printf("Target found in %d steps.\n", stepmat[newx][newy]);
+			}
+
+      	}
+		
+		//printf("\n");
+	}
+
+	//queue is empty, search is complete, reset the visited flag        
+   for(i = 0; i < MAX; i++) 
+   		for (j = 0; j < MAX; j++)
+      		visited[i][j] = NOT_VISITED;
+}
+
+void main() {
+
+	int a[MAX][MAX] = { { 1, 0, 0, 0, 0, 0 },
+						{ 1, 1, 1, 1, 1, 0 },
+						{ 0, 1, 0, 0, 1, 0 },
+						{ 0, 1, 0, 1, 1, 0 },
+						{ 0, 1, 1, 1, 1, 1 },
+						{ 0, 1, 1, 0, 0, 1 } };	
+
+	int initx = 0;
+	int inity = 0;
+	int targetx = 5;
+	int targety = 5;
+
+	print_helper(a, initx, inity, targetx, targety);
+
+	//dfs(a, initx, inity, targetx, targety, 0);
+	bfs(a, initx, inity, targetx, targety, 0);
+}
---
+++ linked.c
@@ -0,0 +1,277 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct list {
+	int current;
+	struct list* next;
+};
+
+typedef struct list list_t;
+
+void insert_begin(list_t** l, int n) {
+	printf("Adding element %d to list.\n", n);
+
+	if ((*l) == NULL) {
+		printf("List NULL. Malloccing first.\n");
+		*l = malloc(sizeof(list_t));
+        assert((*l) != NULL);
+
+		(*l)->current = n;
+		(*l)->next = NULL;
+		printf("Made current %d.\n", (*l)->current);
+	}
+	else {
+		list_t* new = malloc(sizeof(list_t));
+		new->current = n;
+		new->next = *l;
+		*l = new;
+	}
+};
+
+void insert_end(list_t** l, int n) {
+	printf("Adding element %d at end of list.\n", n);
+
+	if ((*l) == NULL) {
+		printf("List NULL. Malloccing first.\n");
+		*l = malloc(sizeof(list_t));
+        assert((*l) != NULL);
+
+		(*l)->current = n;
+		(*l)->next = NULL;
+		printf("Made current %d.\n", (*l)->current);
+	}
+	else {
+		/* iterate through nodes till the end and add it there */
+		list_t* new = malloc(sizeof(list_t));
+		new->current = n;
+		list_t* current = *l;
+
+		while (current->next != NULL) {
+			current = current->next;
+		}
+
+		current->next = new;
+	}	
+}
+
+void delete(list_t** l, int n) {
+	list_t* p = *l;
+	list_t* prev = NULL;
+
+	while ((p->next != NULL) && (p->current != n)) {
+		prev = p;
+		p = p->next;
+	}
+
+	if (p->current == n) {
+		/* three cases, beginning , middle and end */
+		if ((prev != NULL) && (p->next != NULL)) {
+			prev->next = p->next;
+			free(p);
+			return;
+		}
+		if (prev == NULL) {
+			/* at beginning, just delete node */
+			(*l) = p->next;
+			free(p);
+			return;
+		}
+		if (p->next == NULL) {
+			prev->next = NULL;
+			free(p);
+			return;
+		}
+	}
+}
+
+void delete_list(list_t** l) {
+
+	list_t* p = *l;
+	list_t* next;
+
+	while(p != NULL) {
+		next = p->next;
+		free(p);
+		p = next;
+	} 
+
+	*l = NULL;
+}
+
+void merge(list_t** l, int left, int mid, int right) {
+
+	int margin1 = mid - left + 1;
+	int margin2 = right - mid;
+
+	list_t* temp = *l;
+	list_t* temp2 = *l;
+
+	int i, j, k;
+
+	int L[margin1];
+	int R[margin2];
+
+	printf("\n\n");
+
+	for (i = 0; i < left; i++)
+		temp = temp->next;
+	for (i = 0; i < margin1; i++) {
+		L[i] = temp->current; // a[l+i]
+		printf("L(%d)=%d,", i, L[i]);
+		temp = temp->next;
+	}
+
+	temp = *l;
+	for (j = 0; j <= mid; j++)
+		temp2 = temp2->next;
+	for (j = 0; j < margin2; j++) {
+		R[j] = temp2->current;// a[m+1+j]
+		printf("R(%d)=%d,", j, R[j]);
+		temp2 = temp2->next;
+	}
+
+	i = 0;
+	j = 0;
+	k = left;
+
+	temp = *l;
+	for (j = 0; j < left; j++)
+		temp = temp->next;
+	printf("\nstarting from temp = %d\nL[i]=", temp->current);
+	for (i = 0; i < margin1; i++) {
+		printf("%d ", L[i]);
+	}
+	printf("\nR[i]=");
+	for (j = 0; j < margin2; j++) {
+		printf("%d ", R[j]);
+	}
+
+	while ((i < margin1) && (j < margin2)) {
+		if (L[i] <= R[j]) {
+			temp->current = L[i];
+			i++;
+		}
+		else {
+			temp->current = R[j];
+			j++;
+		}
+		temp = temp->next; // k++
+
+	}
+
+	printf("done to here \n");
+
+	/* copy remaining elements if L size != R size */
+	while (i < margin1)
+	{
+		temp->current = L[i];
+		i++;
+		temp = temp->next; // k++
+	}
+	while (j < margin2) {
+		temp->current = R[j];
+		j++;
+		temp = temp->next; // k++
+	}
+
+}
+
+void merge_sort(list_t** l, int left, int right) {
+	if (right > left) {
+		int mid = (left + right) / 2;
+
+		merge_sort(l, left, mid);
+		merge_sort(l, mid + 1, right);
+
+		merge(l, left, mid, right);
+	}
+}
+
+int list_size(list_t* l) {
+	int count = 0;
+
+	while ((l != NULL)) {
+		count++;
+		l = l->next;
+	}
+
+	return count;
+}
+
+void sort_list(list_t** l) {
+	int size = list_size(*l);
+	printf("List size is: %d\n", size);
+	merge_sort(l, 0, size - 1);
+}
+
+/* prints copy of list */
+void printList(list_t* l) {
+
+	printf("List is: ( ");
+
+	while (l != NULL) {
+		printf("%d ", l->current);
+		l = l->next;
+	}
+
+	printf(")\n");
+}
+
+void main() {
+	/* Build simple linked list */
+	/* Reverse linked list */
+	/* Detect loop linked list */
+	/* Add, delete elements from list */
+	list_t* nlist = NULL;
+
+	printList(nlist);
+
+	insert_begin(&nlist, 1);
+	insert_begin(&nlist, 2);
+	insert_begin(&nlist, 3);
+	insert_begin(&nlist, 5);
+
+	/* should show 5,3,2,1*/
+	printList(nlist);
+
+	insert_end(&nlist, 10);
+	insert_end(&nlist, 20);
+	insert_end(&nlist, 30);
+
+	/* should show 5,3,2,1,10,20,30 */
+	printList(nlist);
+
+	delete(&nlist, 20);
+
+	/* should show 5,3,2,1,10,30 */
+	printList(nlist);
+
+	delete(&nlist, 30);
+
+	/* should show 5,3,2,1,10 */
+	printList(nlist);
+
+	delete(&nlist, 5);
+
+	/* should show 3,2,1,10 */
+	printList(nlist);
+
+	insert_begin(&nlist, 5);
+	insert_begin(&nlist, 20);
+	insert_begin(&nlist, 30);
+
+	/* should show 30,20,5,3,2,1,10 */
+	printList(nlist);
+
+	printf("List size is: %d\n", list_size(nlist));
+	/*sort_list(&nlist);*/
+
+	/* should show 1,2,3,5,10,20,30 */
+	printList(nlist);
+
+	delete_list(&nlist);
+
+	printf("Deleted list.\n");
+	printList(nlist);
+}
